032f243b78b8623ad138670538e087bbb591be17
azizbenzarti <azizbenzarti74@gmail.com>
2024-09-14 13:44:04 +0200

    k8s deployment

---
+++ Terraform/outputs.tf
@@ -0,0 +1,18 @@
+output "cluster_name" {
+  value=google_container_cluster.default.name
+}
+
+output "cluster_endpoint" {
+value = google_container_cluster.default.endpoint
+  
+}
+
+output "cluster_location" {
+value = google_container_cluster.default.location
+  
+}
+
+output "load_balancer_ip" {
+value = google_container_cluster.default.address
+  
+}
